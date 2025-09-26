842df96d12eb5dfac8b8cf0a62c88852f87b6c4b
Mehmet <mehmet@katircioglu61.de>
2025-09-26 17:10:29 +0200

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Test from "./pages/Test";
+
 
 function App() {
   
@@ -9,7 +9,6 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/test" element={<Test />} />
       </Routes>
     </>
   )

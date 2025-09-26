0c72608c907d6d01fca33e1f04ec19932636f576
Mehmet <mehmet@katircioglu61.de>
2025-09-26 16:30:59 +0200

    test commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Home(){
+    return(
+        <>
+        <h2>Home</h2>
+        <Link to="/test">Test</Link>
+        </>
+    )
+}
+
+export default Home;

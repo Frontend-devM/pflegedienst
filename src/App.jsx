081c7c1a0885c9598fa094235de110efb18cb566
Mehmet <mehmet@katircioglu61.de>
2025-09-26 18:42:11 +0200

    Lenis import

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import { useEffect } from "react";nh
+import Lenis from "@studio-freight/lenis";
+
 
 
 function App() {
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    // 🔥 Damit Lenis global verfügbar ist:
+    window.lenis = lenis;
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
   
 
   return (
